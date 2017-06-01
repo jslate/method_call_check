ce72b456488597e16bcfabc1a3b820b0c8499d81
Jonathan Slate <jslate@patientslikeme.com>
2017-06-01 11:56:32 -0400

    adding more qualifying of method names which should fix the tests

--- test/method_call_check_test.rb
+++ test/method_call_check_test.rb
@@ -48,23 +48,23 @@ class MethodCallCheckTest < Minitest::Test
   end
 
   def test_class_method_registered
-    assert_equal true, MethodCallCheck::Store.class_method_registered?(:class_method_to_test)
-    assert_includes (Time.now.to_i - 10)..(Time.now.to_i), MethodCallCheck::Store.class_method_registered_at(:class_method_to_test)
+    assert_equal true, MethodCallCheck::Store.class_method_registered?("MethodCallCheckTest::TestObject::class_method_to_test")
+    assert_includes (Time.now.to_i - 10)..(Time.now.to_i), MethodCallCheck::Store.class_method_registered_at("MethodCallCheckTest::TestObject::class_method_to_test")
   end
 
   def test_class_method_called
     reset_calls
-    assert_nil MethodCallCheck::Store.stored_class_method_call_stacks(:class_method_to_test)[0]
+    assert_nil MethodCallCheck::Store.stored_class_method_call_stacks("MethodCallCheckTest::TestObject::class_method_to_test")[0]
     TestObject.class_method_to_test
-    refute_nil MethodCallCheck::Store.stored_class_method_call_stacks(:class_method_to_test)[0]
-    assert_match 'method_call_check_test.rb', MethodCallCheck::Store.stored_class_method_call_stacks(:class_method_to_test)[0]
-    assert_equal 1, MethodCallCheck::Store.stored_class_method_call_count(:class_method_to_test)
+    refute_nil MethodCallCheck::Store.stored_class_method_call_stacks("MethodCallCheckTest::TestObject::class_method_to_test")[0]
+    assert_match 'method_call_check_test.rb', MethodCallCheck::Store.stored_class_method_call_stacks("MethodCallCheckTest::TestObject::class_method_to_test")[0]
+    assert_equal 1, MethodCallCheck::Store.stored_class_method_call_count("MethodCallCheckTest::TestObject::class_method_to_test")
   end
 
   def test_class_method_called_multiple_times
     reset_calls
     6.times { TestObject.class_method_to_test }
-    assert_equal 6, MethodCallCheck::Store.stored_class_method_call_count(:class_method_to_test)
+    assert_equal 6, MethodCallCheck::Store.stored_class_method_call_count("MethodCallCheckTest::TestObject::class_method_to_test")
   end
 
   def test_class_method_called_trucated
@@ -82,8 +82,8 @@ class MethodCallCheckTest < Minitest::Test
     TestObject.class_method_to_test
     TestObject.class_method_to_test
     TestObject.class_method_to_test
-    assert_equal 12, MethodCallCheck::Store.stored_class_method_call_count(:class_method_to_test)
-    assert_equal 10, MethodCallCheck::Store.stored_class_method_call_stacks(:class_method_to_test).count
+    assert_equal 12, MethodCallCheck::Store.stored_class_method_call_count("MethodCallCheckTest::TestObject::class_method_to_test")
+    assert_equal 10, MethodCallCheck::Store.stored_class_method_call_stacks("MethodCallCheckTest::TestObject::class_method_to_test").count
   end
 
   class TestObject
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,13 +6,13 @@ require 'redis'
 def reset_calls
   MethodCallCheck::Store.client.del("method_call_check:instance_methods:counts:MethodCallCheckTest::TestObject::method_to_test")
   MethodCallCheck::Store.client.del("method_call_check:instance_methods:calls:MethodCallCheckTest::TestObject::method_to_test")
-  MethodCallCheck::Store.client.del("method_call_check:class_methods:counts:class_method_to_test")
-  MethodCallCheck::Store.client.del("method_call_check:class_methods:calls:class_method_to_test")
+  MethodCallCheck::Store.client.del("method_call_check:class_methods:counts:MethodCallCheckTest::TestObject::class_method_to_test")
+  MethodCallCheck::Store.client.del("method_call_check:class_methods:calls:MethodCallCheckTest::TestObject::class_method_to_test")
 end
 
 def reset_registrations
   MethodCallCheck::Store.client.del("method_call_check:instance_methods:registered_at:MethodCallCheckTest::TestObject::method_to_test")
-  MethodCallCheck::Store.client.del("method_call_check:class_methods:registered_at:class_method_to_test")
+  MethodCallCheck::Store.client.del("method_call_check:class_methods:registered_at:MethodCallCheckTest::TestObject::class_method_to_test")
 end
 
 reset_calls
